cf9edc5a524740721ead3c52b50cc8c2389c416a
Simon Ng <simonng@appcoda.com>
2017-07-30 12:17:30 +0800

    Initial version

---
+++ HapticFeedback/Feedback.swift
@@ -0,0 +1,9 @@
+//
+//  Feedback.swift
+//  HapticFeedback
+//
+//  Created by Charles Truluck on 7/25/17.
+//  Copyright © 2017 Porter-Gaud School. All rights reserved.
+//
+
+import UIKit
--- HapticFeedback/ViewController.swift
+++ HapticFeedback/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let impact = UIImpactFeedbackGenerator()
+    let selection = UISelectionFeedbackGenerator()
+    let notification = UINotificationFeedbackGenerator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func onePressed(_ sender: UIButton) {
+        impact.impactOccurred()
+    }
+    
+    @IBAction func twoPressed(_ sender: UIButton) {
+        selection.selectionChanged()
+    }
+    
+    @IBAction func successPressed(_ sender: Any) {
+        notification.notificationOccurred(.success)
+    }
+    
+    @IBAction func warningPressed(_ sender: Any) {
+        notification.notificationOccurred(.warning)
+    }
+    
+    @IBAction func errorPressed(_ sender: Any) {
+        notification.notificationOccurred(.error)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
